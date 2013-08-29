c105d905fd7f0c57656771e360de4a5d662c80da
amrta11 <amrta.kf@gmail.com>
2013-08-29 15:32:45 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weekend::Application.config.secret_key_base = '1a5924c23f87c3636cb209490e7d9eff90c9e20c77ef226b2d59d727a8841a3bf757cb8565ffec77b17944c58999c03a89d3ece9233fc30c39d53cd49e812ee5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Weekend::Application.config.session_store :cookie_store, key: '_weekend_session'
